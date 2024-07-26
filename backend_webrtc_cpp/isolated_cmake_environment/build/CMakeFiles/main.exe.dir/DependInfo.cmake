1fb163ecded6c71e8659f0e3c699269d1064c298
Deepraj Das <mail30deeprajdas@gmail.com>
2024-07-27 03:45:40 +0530

    ws done

---
+++ backend_webrtc_cpp/isolated_cmake_environment/build/CMakeFiles/main.exe.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kim/Documents/code/miod/mio/backend_webrtc_cpp/isolated_cmake_environment/src/main.cpp" "CMakeFiles/main.exe.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.exe.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ backend_webrtc_cpp/isolated_cmake_environment/build/CMakeFiles/main.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.exe.dir/src/main.cpp.o"
+  "CMakeFiles/main.exe.dir/src/main.cpp.o.d"
+  "main.exe"
+  "main.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
