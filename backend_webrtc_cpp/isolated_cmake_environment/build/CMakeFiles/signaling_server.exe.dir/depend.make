2f6e69ba51e98cc46caeb4bfa071be13d2b5acaf
Deeprajdas <mail30deeprajdas@gmail.com>
2024-07-27 13:02:35 +0530

    ws working

--- backend_webrtc_cpp/isolated_cmake_environment/build/CMakeFiles/signaling_server.exe.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for signaling_server.exe.
-# This may be replaced when dependencies are built.
--- backend_webrtc_cpp/isolated_cmake_environment/build/cmake_install.cmake
+++ backend_webrtc_cpp/isolated_cmake_environment/build/cmake_install.cmake
@@ -42,26 +42,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kim/Documents/code/mio/backend_webrtc_cpp/isolated_cmake_environment/build/_deps/fmt-build/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kim/Documents/code/mio/backend_webrtc_cpp/isolated_cmake_environment/build/_deps/boost-build/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kim/Documents/code/mio/backend_webrtc_cpp/isolated_cmake_environment/build/_deps/rtaudio-build/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/kim/Documents/code/mio/backend_webrtc_cpp/isolated_cmake_environment/build/_deps/sndfile-build/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
